188b4737a1d60cb403739ab346e8279b37b12fa7
storyofgold <ilhamfaqqotz@gmail.com>
2025-08-22 15:42:52 +0700

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // PENTING: Perbarui ini dengan nama repositori GitHub Anda.
   // Contoh: jika URL repo Anda adalah https://github.com/user/my-app, atur base menjadi '/my-app/'
-  base: '/market-analysis/',
+  base: '/simple-chart/',
 })
